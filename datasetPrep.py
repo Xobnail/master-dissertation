69133d9d6f0d84446eee01d5501dd2e9128e77ae
Nail Khabibullin <Xobnail@gmail.com>
2023-02-11 19:34:52 +0300

    use Uchidalab instead of Tsaug, fixed formatting

--- datasetPrep.py
+++ datasetPrep.py
@@ -1,58 +1,53 @@
 # %%
 import os
 import numpy as np
-from tsaug import Drift, AddNoise
+import time_series_augmentation.utils.augmentation as aug
+# from tsaug import Drift, AddNoise
 from tsaug.visualization import plot
 
 normFolder = '../Src/Норма/onlyTxt/'
 deviationFolder = '../Src/Отклонение/onlyTxt/'
 
-noiseAugmenter = AddNoise(0.015, 0.025)
-
-# TODO: check nan values in source files
-# and fix formatting float numbers 
+# noiseAugmenter = AddNoise(0.015, 0.025)
 
 # %%
 i = 0
-for fileName in os.listdir(normFolder):    
+for fileName in os.listdir(normFolder):
     if not fileName.startswith('aug_'):
         normFile = os.path.join(normFolder, fileName)
 
-        if os.path.isfile(normFile):     
+        if os.path.isfile(normFile):
             X = np.loadtxt(normFile)
-            X_aug = noiseAugmenter.augment(X)
+            X_aug_jitter = aug.jitter(X, sigma=0.005)
+            X_aug_warp = aug.magnitude_warp(X_aug_jitter)
 
             with open(normFolder + 'aug_' + fileName, 'w') as aug_file:
-                np.savetxt(aug_file, [X_aug], delimiter=' ')
+                np.savetxt(aug_file, [X_aug_warp], delimiter=' ', fmt='%01.16f')
 
             if i < 2:
                 print(normFile)
                 print("Before:")
                 plot(X)
                 print("After:")
-                plot(X_aug)  
-            
-            if i < 25:
-                print(X.size, X_aug.size)
+                plot(X_aug_warp)
+
+            # if i < 25:
+            #     print(X.size, X_aug.size)
 
         i += 1
 
-# %%
+# # %%
+# directory = '../Src/Норма/onlyTxt/'
 
-directory = '../Src/Норма/onlyTxt/'
+# X1 = np.loadtxt(directory + "20210917-181304_0.txt")
 
-X1 = np.loadtxt(directory + "20210917-143238_0.txt")
-X2 = np.loadtxt(directory + "20210917-143238_1.txt")
-print("Before:")
-plot(X1)
-plot(X2)
+# i = 0
+# while i < 5:
+#     X1[i] = np.nan
+#     i += 1
+
+# X_aug_test = aug.jitter(X1, sigma=0.005)
+# print(X_aug_test)
+# plot(X1)
+# plot(X_aug_test)
 # %%
-driftAugmenter = AddNoise(0.015, 0.025)
-X1_aug = driftAugmenter.augment(X1)
-X2_aug = driftAugmenter.augment(X2)
-print("After:")
-plot(X1_aug)
-plot(X2_aug)
-# %%
-Drift(max_drift=0.3, n_drift_points=3)
-AddNoise(0.015, 0.025)
